a4426aa5a69cd95cabb514acf0decfe783fbf6de
Jake Jones <jaj2610@gmail.com>
2013-09-07 16:44:04 -0400

    filepicker works for attachments; need to bug check sendgrid

--- js/email.js
+++ js/email.js
@@ -1,14 +1,39 @@
-npm install sendgrid
+filepicker.setKey('AQ6kiiJGJS1iZl7OgXeAPz');
 
-function sendEmail(toList, userEmail, subjectText, emailText)
+var emailObject = new Email();
+
+function addAttachment()
 {
+	filepicker.pick({
+    mimetypes: ['image/*', 'text/plain'],
+    container: 'window',
+    services:['COMPUTER'],
+  	},
+  function(InkBlob){
+    console.log(JSON.stringify(InkBlob));
+  },
+  function(FPError){
+    console.log(FPError.toString());
+  }
+);
+
+	emailObject.addFile({
+		filename: InkBlob.filename,
+		url: InkBlob.url
+	});
+}
+
+function sendEmail()
+{
+	var emailObject = ({
+		to: document.getElementById('to').value,
+		from: document.getElementById('from').value,
+		subject: document.getElementById('subject').value,
+		text: document.getElementById('body').value,
+	});
+
 	var sendgrid = require('sendgrid')(jaj2610, statefarm);
-	sendgrid.send({
-		to: toList,
-		from: userEmail,
-		subject: subjectText,
-		text: emailText
-	}, function(err, json) {
+	sendgrid.send(emailObject, function(err, json) {
 		if (!err) { return console.error(err); }
 			console.log(err);
 	});
