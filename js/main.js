c9c980eb70062f3d4abd967e70e6f81d51267bc5
emilylast <last.emily@gmail.com>
2013-09-08 00:15:36 -0400

    fixed index page

--- js/main.js
+++ js/main.js
@@ -1,18 +1,3 @@
-var myClubs = new Array();
-myClubs[0] = "AWC";
-myClubs[1] = "ACM";
-
-var myEvents = new Array();
-myEvents[0] = "Grace Hopper";
-myEvents[1] = "CodePSU";
-
-var mySponsors = new Array();
-mySponsors[0] = "NSA";
-mySponsors[1] = "CSE Department";
-
-var ACMSponsors = newArray();
-ACMSponsors[0] = "We're Broke";
-
 $(function() {
 	$( "#tabs" ).tabs();
 	$( "#accordion" ).accordion({
