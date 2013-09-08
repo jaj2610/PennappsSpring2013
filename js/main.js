7a771c14b8fd33e1be90b3b6f88f472522e4cce0
emilylast <last.emily@gmail.com>
2013-09-08 04:53:25 -0400

    jquery

--- js/main.js
+++ js/main.js
@@ -1,12 +1,24 @@
 $(function() {
 	$( "#tabs" ).tabs();
 	$( "#eventaccordion" ).accordion({
-		collapsible: true
+		collapsible: true,
+		heightStyle: "fill"
 	});
 });
 
 $(function() {
 	$( "#sponsoraccordion" ).accordion({
-	collapsible: true
+	collapsible: true,
+	heightStyle: "fill"
 	});
+});
+
+$(function() {
+$( "#accordion-resizer" ).resizable({
+minHeight: 400,
+minWidth: 200,
+resize: function() {
+$( "#sponsoraccordion" ).accordion( "refresh" );
+}
+});
 });
